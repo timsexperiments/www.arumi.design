fbd50e7f62711654c6634ea25fa6d06d3ba2237e
Tim <76897954+timsexperiments@users.noreply.github.com>
2024-02-08 21:15:05 +0000

    add cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arumi.design',
-  integrations: [mdx(), sitemap(), tailwind(), icon()]
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  output: "server",
+  adapter: cloudflare()
 });
