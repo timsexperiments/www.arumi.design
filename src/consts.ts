1802bf7a9f5e87d5299311b6875dc8b412bf215c
Tim <tim@timsexperiments.foo>
2024-02-08 19:13:57 -0600

    Create socials component and update links to use the constant values

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,6 @@
 export const SITE_TITLE = 'Arumi Design';
 export const SITE_DESCRIPTION =
   'Designing with purpose, Delivering with impact.';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/arumi-choudhary/';
+export const INSTAGRAM_URL = 'https://www.instagram.com/designsbyarumi/';
+export const TIKTOK_URL = 'https://www.tiktok.com/@designsbyarumi/';
