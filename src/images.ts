ad7db6359efa81f1deebffe98af9a491266de954
Tim <tim@timsexperiments.foo>
2024-02-08 21:58:24 -0600

    Reorganize images

--- src/images.ts
+++ src/images.ts
@@ -1,13 +1,27 @@
 import type { ImageMetadata } from 'astro';
 
 export async function imagePreview(slug: string) {
-  const imagePath = `/src/assets/${slug}-preview.png`;
+  const imagePath = `/src/assets/preview/${slug}.png`;
   const images = import.meta.glob<{ default: ImageMetadata }>(
-    '/src/assets/*.{jpeg,jpg,png,gif}',
+    '/src/assets/preview/*.{jpeg,jpg,png,gif}',
   );
-  if (!images[imagePath])
+  if (!images[imagePath]) {
     throw new Error(
-      `"${imagePath}" does not exist in glob: "src/assets/*.{jpeg,jpg,png,gif}"`,
+      `"${imagePath}" does not exist in glob: "src/assets/preview/*.{jpeg,jpg,png,gif}"`,
     );
+  }
+  return images[imagePath]();
+}
+
+export async function imageHero(slug: string) {
+  const imagePath = `/src/assets/hero/${slug}.png`;
+  const images = import.meta.glob<{ default: ImageMetadata }>(
+    '/src/assets/hero/*.{jpeg,jpg,png,gif}',
+  );
+  if (!images[imagePath]) {
+    throw new Error(
+      `"${imagePath}" does not exist in glob: "src/assets/hero/*.{jpeg,jpg,png,gif}"`,
+    );
+  }
   return images[imagePath]();
 }
