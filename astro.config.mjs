33a894c6165d882e7c48caf2bc9f8eeabbf3fa8a
Tim <tim@timsexperiments.foo>
2024-02-18 10:31:42 -0600

    Update astro configuration for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arumi.design',
   integrations: [mdx(), sitemap(), tailwind(), icon()],
-  output: 'server',
-  adapter: cloudflare({ imageService: 'passthrough' }),
+  output: 'hybrid',
+  image: {
+    service: sharpImageService(),
+  },
+  adapter: cloudflare(),
 });
