c4d685317b0b162ba7317728f29f2e99d36d9959
Tim <tim@timsexperiments.foo>
2024-02-22 11:38:39 -0600

    Move project protection to middleware

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import plugin from 'tailwindcss/plugin';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
   theme: {
     fontFamily: {
       display: [
