72d82326765042859711ca38cf8538692b0078cf
Tim <tim@timsexperiments.foo>
2024-03-18 23:20:20 -0500

    Update images to webp instead of png

--- src/images.ts
+++ src/images.ts
@@ -1,26 +1,26 @@
 import type { ImageMetadata } from 'astro';
 
 export async function imagePreview(slug: string) {
-  const imagePath = `/src/assets/preview/${slug}.png`;
+  const imagePath = `/src/assets/preview/${slug}.webp`;
   const images = import.meta.glob<{ default: ImageMetadata }>(
-    '/src/assets/preview/*.{jpeg,jpg,png,gif}',
+    '/src/assets/preview/*.{jpeg,jpg,png,webp,gif}',
   );
   if (!images[imagePath]) {
     throw new Error(
-      `"${imagePath}" does not exist in glob: "src/assets/preview/*.{jpeg,jpg,png,gif}"`,
+      `"${imagePath}" does not exist in glob: "src/assets/preview/*.{jpeg,jpg,png,webp,gif}"`,
     );
   }
   return images[imagePath]();
 }
 
 export async function imageHero(slug: string) {
-  const imagePath = `/src/assets/hero/${slug}.png`;
+  const imagePath = `/src/assets/hero/${slug}.webp`;
   const images = import.meta.glob<{ default: ImageMetadata }>(
-    '/src/assets/hero/*.{jpeg,jpg,png,gif}',
+    '/src/assets/hero/*.{jpeg,jpg,png,webp,gif}',
   );
   if (!images[imagePath]) {
     throw new Error(
-      `"${imagePath}" does not exist in glob: "src/assets/hero/*.{jpeg,jpg,png,gif}"`,
+      `"${imagePath}" does not exist in glob: "src/assets/hero/*.{jpeg,jpg,png,webp,gif}"`,
     );
   }
   return images[imagePath]();
